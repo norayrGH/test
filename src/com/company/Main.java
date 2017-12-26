93d88e0851c7fdaa23f89e205ecd9ee0ed4473c8
noro741 <norayr.gharibyan.java@gmail.com>
2017-12-26 22:56:40 +0400

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main extends Thread {
+
+    public static void main(String[] args) {
+        String str = "StudyTonight";
+        str.concat(".com");
+        str = str.toUpperCase();
+        str.replace("TONIGHT","today");
+        System.out.println(str);
+    }
+    public void run()
+    {
+        System.out.print("Thread ");
+    }
+}
